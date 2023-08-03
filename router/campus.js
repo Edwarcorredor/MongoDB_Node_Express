32b9312ab67889d9b25463081bbf5f7ff11ba637
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-08-03 17:21:37 -0500

    feat:

--- router/campus.js
+++ router/campus.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { limitGet } from "../limit/config.js";
+import { con } from "../db/atlas.js";
+import { ObjectId } from "bson";
 const appCampus = Router();
 
-appCampus.get('/', limitGet(), (req, res) => {
+appCampus.get('/', limitGet(), async (req, res) => {
     
     if(!req.rateLimit) return
-
-    res.send(":)");
+    let { id } = req.body;
+    let db = await con();
+    let usuario = db.collection('usuario');
+    let result = await usuario.find({_id: new ObjectId(id)}).toArray();
+    res.send(result);
     
 })
 
